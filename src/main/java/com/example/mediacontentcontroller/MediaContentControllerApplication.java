f13d94ba2f503b7df45a1d0568c266f1cf918cf6
Rex <lamyipan0225@gmail.com>
2022-10-25 12:46:16 -0400

    spring boot initializr

---
+++ src/main/java/com/example/mediacontentcontroller/MediaContentControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediacontentcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaContentControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaContentControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mediacontentcontroller/MediaContentControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediacontentcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaContentControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
